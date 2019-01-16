a9e8d43d4aa123b1e542e5198bfc18ad4c563d7e
Patrick Lee <patrick@noom.com>
2019-01-16 10:06:07 -0500

    Parse existing todo items and copy it over.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 import sys
 import json
 import subprocess
 import requests
 from datetime import datetime
+from dateutil import parser
 
 # Add xcall folder to path so we can import it since
 # we're using it as a submodule.
@@ -20,7 +22,10 @@ PLANNER_TAG = "day planner"
 TITLE_DAY_FORMAT = "%A %B %-d, %Y"
 NOTE_BODY = u"""
 ## Todo:
-- [ ] Do something
+{todo_items}
+
+## Notes:
+
 
 ## Today's Agenda:
 {agenda}
@@ -44,7 +49,11 @@ def format_note():
     agenda = get_daily_agenda()
     weather = get_weather()
 
-    body = NOTE_BODY.format(agenda=agenda, weather=weather)
+    todos = get_unfinished_todo_items()
+    if not todos:
+        todos = ["- [] Start doing something"]
+
+    body = NOTE_BODY.format(agenda=agenda, weather=weather, todo_items="\n".join(todos))
 
     return {
         "title": title,
@@ -65,6 +74,39 @@ def get_existing_notes():
     return None
 
 
+def get_last_note():
+    contents = xcall.xcall('bear', 'search', {"tag": PLANNER_TAG, "token": BEAR_API_TOKEN, "show_window": "no"})
+    notes = json.loads(contents['notes'])
+    last_note = None
+    last_note_creation_date = None
+    for note in notes:
+        creation_date = parser.parse(note['creationDate'])
+        if not last_note:
+            last_note = note
+            last_note_creation_date = creation_date
+        else:
+            if creation_date > last_note_creation_date:
+                last_note = note
+                last_note_creation_date = creation_date
+    note = xcall.xcall('bear', 'open-note', {"id": last_note['identifier'], "new_window": "no", "show_window": "no"})
+    return note
+
+
+def get_unfinished_todo_items():
+    last_note = get_last_note()
+    note_body = last_note['note']
+    lines = note_body.split('\n')
+    pattern = re.compile('(?:- \[ \] )(.*)')
+
+    existing_todos = []
+    for line in lines:
+        match = pattern.match(line)
+        if match:
+            existing_todos.append(match.group(0))
+
+    return existing_todos
+
+
 def create_daily_note():
     parameters = format_note()
     contents = xcall.xcall('bear', 'create', parameters)
@@ -72,6 +114,8 @@ def create_daily_note():
 
 
 def main():
+    # TODO: Get unchecked todo items from previous day and add them to current todo.
+    # TODO: Remove morning time and evening time from calendar.
     if not get_existing_notes():
         create_daily_note()
         print("Daily note created!")
